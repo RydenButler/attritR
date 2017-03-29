edf3d6326c71f372cf8f14d17bee8815e70062b9
RydenButler <ryden.winfield@gmail.com>
2017-03-29 13:01:42 -0500

    estimateWeights
    - Specify calculateWeights function
    - Move old code to ExtraCode file outside of package
    - Add basic simulated data to dev file

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -3,3 +3,37 @@ library(roxygen2)
 
 # DO NOT RUN THE FOLLOWING LINE
 #### package.skeleton('estimateDelta')
+
+# Compile latest package and re-document
+Current <- as.package('estimateDelta')
+load_all(Current)
+document(Current)
+
+devtools::use_package('stats')
+
+# Create vector of treatment indicators
+D <- sample(c(0, 1), 100 , replace = T)
+# Create vector of covariates
+# !!! We'll need to make this work with dataframes
+X <- rnorm(100, 0, 3)
+# Create vector of instruments
+Z <- sample(1:5, 100, replace = T, prob = c(0.3, 0.2, 0.2, 0.2, 0.1))
+# Create vector of Y values, related to D and X
+Y <- 5*D + 2*X + rnorm(100, 0, 3)
+# Create probabilistic attrtion vector
+Attrition <- rnorm(100, Z, 5)
+# Make sure that this isn't totally deterministic for Z == 5
+order(Attrition, decreasing=TRUE)[1:15]
+which(Z == 5)
+# Change 10 largest attrition values in Y to NA
+Y[order(Attrition, decreasing=TRUE)[1:15]] <- NA
+
+# A dataframe of covariates for later use 
+#X <- data.frame(Binary = sample(c(0, 1), 100, replace = T), 
+#              Continuous = rnorm(100, 0, 3),
+#              Factor = sample(c('a', 'b', 'c'), 100, replace = T))
+
+# Test calculateWeights
+MyWeights <- calculateWeights(Y = Y, D = D, X = X, Z = Z)
+# Test weight in lm
+summary(lm(Y ~ X + D, weights = MyWeights))
--- estimateDelta/R/calculateWeights.R
+++ estimateDelta/R/calculateWeights.R
@@ -1,30 +1,27 @@
-library(stats)
-
-# refer to EQ 7 in Huber2012
-estimateDelta <- function(Y, X, R, D, Z) {
-  # Input vector of outcomes, Y
-  # Input dataframe of covariates, X
-  # Input vector of responses, R (can deduce from Y == NA)
-  # Input vector of treatment indicators, D
-  # Input vector of instrument values, Z
-  
-  Data <- data.frame(cbind(Y,X,R,D,Z))
-  # Calculate p(W); top pg. 457
-  Data$p_w <- glm(R ~ D + X + Z, 
-                  family = gaussian(link = logit), 
-                  data = Data
-                  )
-  # Calculate pi(X, p(W)); bottom pg. 458
-  pi <- glm(D ~  X + p_w, 
-            family = gaussian(link = logit), 
-            data = Data[Data$R == 1, ]
-            )
-  # Need to convert p_w and pi to fitted values via predict.glm()
-  Treated <- (R*D*Y)/(p_w*pi)
-  Untreated <- (R*(1-D)*Y)/(p_w*(1-pi))
-  delta <- Treated - Untreated
-  
-  
-  ### !!! Jacob claims we need not calculate delta
-  ### We need to estimate p(W) and pi, and use the denominator to weight a regression
+# Note that glm() requires stats package
+calculateWeights <- function(Y, X, D, Z) {
+  # Calculate attrition indicator, R 
+  R <- as.numeric(is.na(Y))
+  # Calculate p(W) - using the instrument
+  p_w <- glm(R ~ D + X + Z, family = binomial(link = logit)) 
+  # Predict values given the model
+  p_w_fits <- predict(object = p_w,
+                      newdata = data.frame(cbind(X, D, Z)),
+                      type = 'response')
+  # Calculate pi(X, p(W)) - using instrumented probabilities and covariates
+  pi <- glm(D ~ X + runif(100,0,1), family = binomial(link = logit), maxit = 1000) 
+  # Predict values given the model
+  pi_fits <- predict(object = pi,
+                     newdata = data.frame(cbind(X, p_w_fits)),
+                     type = 'response')
+  # Note treated respondents
+  Treated <- which(D == 1)
+  # Create empty vector of weights
+  Weights <- rep(NA, length(pi_fits))
+  # Fill in weights for treated respondents
+  Weights[Treated] <- p_w_fits[Treated] * pi_fits[Treated]
+  # Fill in weights for untreated respondents
+  Weights[-Treated] <- p_w_fits[-Treated] * (1 - pi_fits[-Treated])
+  return(Weights)
 }
+
