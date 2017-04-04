5acb1a0abb54380d76473c22843283a369f7c833
hyunoooh <hyunjoo.oh@wustl.edu>
2017-04-04 01:11:12 -0500

    Update documentation of calculateWeights
    1) Change the description of inputs and returns
    2) Added and corrected the comments in the function

--- estimateDelta/R/calculateWeights.R
+++ estimateDelta/R/calculateWeights.R
@@ -1,37 +1,41 @@
 #' Calculating weights for treatment and control group under non-random attrition
 #'
 #' \code{calculateWeights} estimates the weights for treatment and control group 
-#' in order to obtain internally and externally valid estimates for the average treatment
-#' effect in cases of attrition.
+#' in order to obtain internally and externally valid estimates for the average treatment effect (ATE) 
+#' in cases of attrition.
 #' 
-#' @param Y Numeric vector of outcome variable. Non-response should be coded as NA. 
-#' @param D Numeric vector of treatment indicators.
-#' @param X Numeric vector of observed covariates.
-#' @param Z Numeric vector of instrumental variable.
-#' @param method String indicating method of probability estimation. Can be glm or gam.
+#' @param modelData A matrix with columns of \code{Y}, \code{D}, \code{X}, where 
+#' \code{Y} indicates outcome variable which may or may not include NA (non-response), 
+#' \code{D} indicates treatment indicator, either 1 (treatment) or 0 (non-treatment),
+#' \code{X} indicates observed covariates, and \code{Z} indicates instrumental variable. 
+#' \code{modelData} must structured with \code{Y}, \code{D}, \code{X}. 
+#' Otherwise, the incorrect output will be produced.
+#' @param instrumentData A matrix with a column of \code{Z}, indicating instrumental variable. 
+#' \code{Z} is a predictor of response variable (R) of 1 if \code{Y} is observed and 0 otherwise (attribution).
+#' It needs to contain at least one continuous element and not to have a direct effect on the outcome (\code{Y}).
+#' @param method A string indicating the estimation method to be used for generating the weights, 
+#' which can be either generalized linear model ("glm") or generalized additive model ("gam").
+#' The default method is generalized linear model ("glm"), which allows for response variable (R)
+#' to have error distribution other than normal distribution.
+#'
 #' 
 #' @details
-#' The function estimates the response propensity score and the treatment 
-#' propensity score to reweigh the observations and correct for attrition. 
-#' The response propensity score is defined as $p(W) = Pr(R=1 | \code{X},
-#' \code{D}, \code{Z})$ where $R$ denotes to the binary response variable 
-#' being 1 if \code{Y} is observed and 0 otherwise (attrition), and the treatment
-#' propensity score, defined as $\pi(\code{X}, p(W)) = Pr(\code{D}=1 | \code{X}, p(W), R=1)$.
+#' The function estimates the response propensity score and the treatment propensity score 
+#' to reweigh the observations and correct for attrition. 
+#' The response propensity score is defined as $p(W) = Pr(R=1 | \code{X},\code{D}, \code{Z})$ 
+#' where $R$ denotes to the binary response variable, being 1 if \code{Y} is observed and 0 otherwise (attrition), 
+#' and the treatment propensity score, defined as $\pi(\code{X}, p(W)) = Pr(\code{D}=1 | \code{X}, p(W), R=1)$.
 #' Response and treatment propensity scores are multiplied for treatment and control group
-#' in order to adjust for differences in the distributions of X and p(W) between 
-#' treated and nontreated respondents. For more information on the method, 
-#' see Huber (2012) below.
+#' in order to adjust for differences in the distributions of X and p(W) between treated and nontreated. 
+#' For more information on the method, see Huber (2012) below.
 #' 
 #' @references Huber (2012): "Identification of Average Treatment Effects in 
 #' Social Experiments Under Alternative Forms of Attrition.", Journal of 
 #' Educational and Behavioral Statistics, vol. 37, no. 3, 443-474.
 #'
-#' @return A numeric vector of the product of the response propensity score and treatment propensity score 
-#'  \item{Y}{An object of responses which may or may not include NA}
-#'  \item{D}{A numeric vector (binary; treatment = 1, contro = 0)} 
-#'  \item{X}{A numeric vector of observed covariates}
-#'  \item{Z}{A numeric vector of instrumental variable}
-#'  \item{method}{A string indicating the method of probability estimation}
+#' @return A dataframe of estimated weights for treated and untreated(control) group.
+#'  \item{column1}{A dataframe of estimated weights for treated and untreated group, 
+#'  returned by products of predicted values which are calculated by \code{method}.}
 #' @author Ryden Butler, David Miller, Jonas Markgraf, and Hyunjoo Oh
 #' 
 #' @rdname calculateWeights
@@ -39,27 +43,27 @@
 #' @export
 
 calculateWeights <- function(modelData, instrumentData, method = 'glm') {
-  # Recode Y as R
-  # R is test participation: if Y is observed, R=1; else, R=0
+  # Recode Y as a form of R, which denotes a binary response variable i.e., test participation:
+  # if Y is observed (non-attribution), 1; otherwise, 0.
+  # In modelData, the first column has values of R which are recoded binary values of Y.
   modelData[ , 1] <- as.numeric(!is.na(modelData[ , 1]))
   # IMPORTANT NOTE:
   ### This modelData must be structured as columns of Y, D, X, Z
   ### Otherwise the following calculations are incorrect
   
   if(method == 'glm'){
-    #### Calculate response propensity score p(W)
-    # fitting generalized linear model: response propensity R regressed on treatment status,
-    # observed covariates, and instrument
+    #### Calculate response propensity score, p(W)
+    # fitting generalized linear model: 
+    # response propensity R regressed on treatment status (D), observed covariates (X), and instrument (Z)
     p_w <- glm(formula = formula(paste(colnames(modelData)[1], '~ .')), 
                family = binomial(link = logit),
                data = data.frame(modelData, instrumentData)) 
     # Predict values given the model
     p_w_fits <- predict(object = p_w,
-      newdata = data.frame(modelData[, -1], instrumentData),
-      type = 'response')
+                        newdata = data.frame(modelData[, -1], instrumentData),
+                        type = 'response')
     #### Calculate treatment propensity score pi(X, p(W))
-    # fitting GLM: treatment propensity D regressed on instrumented probabilities 
-    # and covariates
+    # fitting GLM: treatment propensity (D) regressed on instrumental variable (D) and covariates (X)
     pi <- glm(formula = formula(paste(colnames(data.frame(modelData[ , -1])), '~ .')), 
               family = binomial(link = logit), 
               maxit = 1000, 
@@ -70,9 +74,9 @@ calculateWeights <- function(modelData, instrumentData, method = 'glm') {
                        type = 'response')
 
   } else {
-    #### Calculate response propensity score p(W)
-    # fitting generalized additive model: response propensity R regressed on treatment status,
-    # observed covariates, and instrument
+    #### Calculate response propensity score, p(W)
+    # fitting generalized additive model: 
+    # response propensity (R) regressed on treatment status (D), observed covariates (X), and instrument (Z)
     p_w <- gam(formula = formula(paste(colnames(modelData)[1], '~ .')), 
                family = binomial(link = logit),
                data = data.frame(modelData, instrumentData))
@@ -82,8 +86,7 @@ calculateWeights <- function(modelData, instrumentData, method = 'glm') {
                         type = 'response')    
     
     #### Calculate treatment propensity score pi(X, p(W))
-    # fitting GAM: treatment propensity D regressed on instrumented probabilities 
-    # and covariates
+    # fitting GAM: treatment propensity (D) regressed on instrumental variable (D) and covariates (X)
     pi <- gam(formula = formula(paste(colnames(data.frame(modelData[ , -1])), '~ .')), 
               family = binomial(link = logit), 
               maxit = 1000,
@@ -101,4 +104,3 @@ calculateWeights <- function(modelData, instrumentData, method = 'glm') {
                                              (1 - pi_fits[which(modelData[ , 2] != 1)]))
   return(Weights)
 }
-
