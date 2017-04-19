ab4b828169e6054c355a6d85a908a1451c624588
jmarkgraf <jonas.markgraf@gmail.com>
2017-04-19 17:49:17 -0500

    Updates for Tests
    Value tests for estimateDelta…done.
    Cosmetic changes in calculateWeights…done.
    
    Not clear yet: “context” for estimateDelta.

--- ATTR/tests/testthat/test-calculateWeights.R
+++ ATTR/tests/testthat/test-calculateWeights.R
@@ -58,68 +58,71 @@ Test.WeightList <- list(pW = TestData$p_W_Fits,
                      pWxPi = Test.AllWeights)
  
 
+# Unit Testing ###############################################
+# ============================================================
 
-
-### Tests:        
-
-test_that("test that calculateWeights generates correct length of vectors", {
-  expect_equal(length(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$pW),
-    expected = length(Test.WeightList$pW))
-  expect_equal(length(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$Pi),
-    expected = length(Test.WeightList$Pi))
-  expect_equal(length(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$pWxPi),
-    expected = length(Test.WeightList$pW))
-})
-
-test_that("calculateWeights produce right values", {
-  # does the function produce right values?
+test_that("calculateWeights calculated correct values", {
+  # testing for pW value
   expect_equal(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$pW,
     expected = Test.WeightList$pW)
+  # testing Pi value
   expect_equal(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$Pi,
     expected = Test.WeightList$Pi)
+  # testing pWxPi value
   expect_equal(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$pWxPi,
     expected = Test.WeightList$pWxPi)
 })
 
-test_that("calculateWeights produce values within a right range", {
-  # does the function produce values within a right range?
-    expect_true(1 %in% findInterval(calculateWeights(modelData = SimData[,1:3],
-                                  instrumentData = SimData[,4])$pW, 
-                             c(0.00001, 0.99999)))
-    expect_true(1 %in% findInterval(calculateWeights(modelData = SimData[,1:3],
-                                  instrumentData = SimData[,4])$Pi, 
-                             c(0.00001, 0.99999)))
-    expect_true(1 %in% findInterval(calculateWeights(modelData = SimData[,1:3],
-                                                     instrumentData = SimData[,4])$pWxPi, 
-                                    c(0.000001, 0.999999)))
-})
-
-test_that("calculateWeights produce an output of right class", {
-  # does the function produce an output of right class?
-  expect_is(calculateWeights(SimData[,1:3], SimData[,4]), "list")
-  expect_is(calculateWeights(SimData[,1:3], SimData[,4])$pW, "numeric")
-  expect_is(calculateWeights(SimData[,1:3], SimData[,4])$Pi, "numeric")
-  expect_is(calculateWeights(SimData[,1:3], SimData[,4])$pWxPi, "numeric")  
-})
-
-test_that("calculateWeights have right number of rows/columns", {
-  # does the function have right number of columns?
-  expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])), 
-               expected = 3)
-  expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])$pW), 
-               expected = nrow(SimData))
-  expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])$Pi), 
-               expected = length(na.omit(SimData[,1])))
-  expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])$pWxPi), 
-               expected = length(na.omit(SimData[,1])))  
-})
-
-test_that("calculateWeights detect error", {
-  # does the function detect error when it should?
-  # case 1: incorrect number of subscripts on matrix
-  expect_error(calculateWeights(SimData[,1], SimData[,4])) # modelData = only Y
-  expect_error(calculateWeights(SimData[,c(1,3)], SimData[,4])) # modelData = (Y, Covariates)
-  expect_error(calculateWeights(SimData[,4], SimData[,1:3])) # modelData = instrument, InstumentData = model
-  # case 2: number of rows does not match
-  expect_error(calculateWeights(SimData[1:(2/length(SimData)),1:3], SimData[,4]))  
-})
+# SAVED TESTS FOR LATER TESTS ---------------------------------------------------------------
+# 
+# test_that("test that calculateWeights generates correct length of vectors", {
+#   expect_equal(length(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$pW),
+#     expected = length(Test.WeightList$pW))
+#   expect_equal(length(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$Pi),
+#     expected = length(Test.WeightList$Pi))
+#   expect_equal(length(calculateWeights(modelData = SimData[,1:3], instrumentData = SimData[,4])$pWxPi),
+#     expected = length(Test.WeightList$pW))
+# })
+# 
+# test_that("calculateWeights produce values within a right range", {
+#   # does the function produce values within a right range?
+#     expect_true(1 %in% findInterval(calculateWeights(modelData = SimData[,1:3],
+#                                   instrumentData = SimData[,4])$pW, 
+#                              c(0.00001, 0.99999)))
+#     expect_true(1 %in% findInterval(calculateWeights(modelData = SimData[,1:3],
+#                                   instrumentData = SimData[,4])$Pi, 
+#                              c(0.00001, 0.99999)))
+#     expect_true(1 %in% findInterval(calculateWeights(modelData = SimData[,1:3],
+#                                                      instrumentData = SimData[,4])$pWxPi, 
+#                                     c(0.000001, 0.999999)))
+# })
+# 
+# test_that("calculateWeights produce an output of right class", {
+#   # does the function produce an output of right class?
+#   expect_is(calculateWeights(SimData[,1:3], SimData[,4]), "list")
+#   expect_is(calculateWeights(SimData[,1:3], SimData[,4])$pW, "numeric")
+#   expect_is(calculateWeights(SimData[,1:3], SimData[,4])$Pi, "numeric")
+#   expect_is(calculateWeights(SimData[,1:3], SimData[,4])$pWxPi, "numeric")  
+# })
+# 
+# test_that("calculateWeights have right number of rows/columns", {
+#   # does the function have right number of columns?
+#   expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])), 
+#                expected = 3)
+#   expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])$pW), 
+#                expected = nrow(SimData))
+#   expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])$Pi), 
+#                expected = length(na.omit(SimData[,1])))
+#   expect_equal(length(calculateWeights(SimData[,1:3], SimData[,4])$pWxPi), 
+#                expected = length(na.omit(SimData[,1])))  
+# })
+# 
+# test_that("calculateWeights detect error", {
+#   # does the function detect error when it should?
+#   # case 1: incorrect number of subscripts on matrix
+#   expect_error(calculateWeights(SimData[,1], SimData[,4])) # modelData = only Y
+#   expect_error(calculateWeights(SimData[,c(1,3)], SimData[,4])) # modelData = (Y, Covariates)
+#   expect_error(calculateWeights(SimData[,4], SimData[,1:3])) # modelData = instrument, InstumentData = model
+#   # case 2: number of rows does not match
+#   expect_error(calculateWeights(SimData[1:(2/length(SimData)),1:3], SimData[,4]))  
+# })
--- ATTR/tests/testthat/test-estimateDelta.R
+++ ATTR/tests/testthat/test-estimateDelta.R
@@ -3,95 +3,167 @@ context("estimateDelta")
 
 ## Estimating delta without using the estimateDelta function:
 
+### Create simulation data, SimData:
+library(MASS)
+set.seed(12345)
+
+Treatment <- rbinom(n = 1000, size = 1, prob = 0.5)
+Covariate <- runif(n = length(Treatment), min = -1, max = 1)
+Instrument <- runif(n = length(Treatment), min = -1, max = 2)
+
+# For attrition on observables
+UV <- mvrnorm(n = 1000, mu = c(0,0), Sigma = matrix(c(1, 0, 0, 1), nrow = 2))
+U <- UV[ , 1]
+V <- UV[ , 2]
+
+Y <- 1*Treatment + 1*Covariate + 0.25*Treatment*Covariate + U
+R <- 1*Treatment + 1*Covariate + 0*Instrument + V > 0
+Y[!R] <- NA
+
+SimData <- data.frame(Y,Treatment,Covariate,Instrument)
+
+## Functions for fitting and working with generalized additive model are required.
+library(gam)
+
 # Extract model data given formula
-TestData.full <- SimData
-TestData.full <- model.frame(Y ~ Treatment + Covariate, TestData.full, na.action = NULL)
+TestData <- SimData
+TestData <- model.frame(Y ~ Treatment + Covariate, TestData, na.action = NULL)
+
 # Extract instrument data given formula
-TestInstrumentData <- model.frame(~ Instrument, TestData.full, na.action = NULL)
-  
-# Calculate weights; add this to data b/c lm() won't recognize the object otherwise
-Test.WeightList <- calculateWeights(modelData = TestData.full, 
-                                    instrumentData = TestInstrumentData)
-TestData.full <- TestData.full[!is.na(TestData.full[ , 1]), ]
-TestData.full$Pi <- Test.WeightList$Pi
-TestData.full$pWxPi <- Test.WeightList$pWxPi
+Test.InstrumentData <- model.frame(~ Instrument, TestData, na.action = NULL)
+
+# Calculate weights -- Imported from 'test-calculateWeights' (see file for commented version) -------------
+
+TestData[ ,1] <- as.numeric(!is.na(.subset2(TestData, 1)))
+names(TestData)[1:2] <- c('R', 'D')
+TestData$p_W_Fits <- predict(object = glm(formula = R ~ Treatment + Covariate + Instrument, 
+  family = binomial(link = logit),
+  data = TestData,
+  maxit = 1000),
+  newdata = TestData,
+  type = 'response')
+TestData[TestData$D != 1, ]$p_W_Fits <- (1 - TestData)[TestData$D != 1, ]$p_W_Fits
+Test.Pi_Fits <- predict(object = glm(formula = D ~ Covariate + p_W_Fits,
+  family = binomial(link = logit),
+  data = TestData,
+  maxit = 1000),
+  newdata = TestData,
+  type = 'response')
+Test.Pi_Fits[TestData$D != 1] <- (1 - Test.Pi_Fits[TestData$D != 1])
+Test.AllWeights <- TestData$p_W_Fits * Test.Pi_Fits
+Test.WeightList <- list(pW = TestData$p_W_Fits, 
+  Pi = Test.Pi_Fits,
+  pWxPi = Test.AllWeights)
+
+# add estimated 'Pi'and 'pWxPi" to TestData
+TestData$Pi <- Test.WeightList$Pi
+TestData$pWxPi <- Test.WeightList$pWxPi
   
 # Estimate Proposition 4:
 Test.RespondentModel <- lm(formula = Y ~ Treatment + Covariate,
                            weights = Pi,
-                           data = TestData.full)
+                           data = TestData)
 # Estimate Proposition 5:
 Test.AllModel <- lm(formula = Y ~ Treatment + Covariate,
                     weights = pWxPi,
-                    data = TestData.full)
+                    data = TestData)
+
 # Final output of estimateDelta:  
 Test.Delta <- list(RespondentDelta = Test.RespondentModel,
                    AllDelta = Test.AllModel)
 
 
-## tests:
-test_that("estimateDelta produce right values", {
-  # does the function produce right values?
-  expect_equal(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                             instrumentFormula = ~ Instrument,
-                             data = SimData),
-               expected = Test.Delta
-  )
+# Unit Testing ###############################################
+# ============================================================
+
+# test estimated values for RespondentDelta
+test_that("Test that estimateDelta gives correct values for RespondentDelta", {
+  # test estimated intercept value
+  expect_equal(unlist(estimateDelta(regressionFormula = Y ~ Treatment + Covariate, 
+    instrumentFormula = ~ Instrument, data = TestData)$RespondentDelta)[1],
+    expected = unlist(Test.Delta$RespondentDelta)[1])
+  # test estimated treatment value
+  expect_equal(unlist(estimateDelta(regressionFormula = Y ~ Treatment + Covariate, 
+    instrumentFormula = ~ Instrument, data = TestData)$RespondentDelta)[2],
+    expected = unlist(Test.Delta$RespondentDelta)[2])
+  # test estimated covariate values
+  expect_equal(unlist(estimateDelta(regressionFormula = Y ~ Treatment + Covariate, 
+    instrumentFormula = ~ Instrument, data = TestData)$RespondentDelta)[3],
+    expected = unlist(Test.Delta$RespondentDelta)[3])
 })
 
-test_that("estimateDelta produce an output of right class", {
-  # does the function produce an output of right class?
-  expect_is(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                          instrumentFormula = ~ Instrument,
-                          data = SimData), 
-            class = "list")
-  expect_is(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                          instrumentFormula = ~ Instrument,
-                          data = SimData)$RespondentDelta, 
-            class = "lm")
-  expect_is(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                          instrumentFormula = ~ Instrument,
-                          data = SimData)$AllDelta, 
-            class = "lm")
+# test estimated values for AllDelta
+test_that("Test that estimateDelta gives correct values for AllDelta", {
+  # test estimated intercept value
+  expect_equal(unlist(estimateDelta(regressionFormula = Y ~ Treatment + Covariate, 
+    instrumentFormula = ~ Instrument, data = TestData)$AllDelta)[1],
+    expected = unlist(Test.Delta$AllDelta)[1])
+  # test estimated treatment value
+  expect_equal(unlist(estimateDelta(regressionFormula = Y ~ Treatment + Covariate, 
+    instrumentFormula = ~ Instrument, data = TestData)$AllDelta)[2],
+    expected = unlist(Test.Delta$AllDelta)[2])
+  # test estimated covariate values
+  expect_equal(unlist(estimateDelta(regressionFormula = Y ~ Treatment + Covariate, 
+    instrumentFormula = ~ Instrument, data = TestData)$AllDelta)[3],
+    expected = unlist(Test.Delta$AllDelta)[3])
 })
 
-test_that("estimateDelta have right number of elements", {
-  # does the function produce both RespondentDelta and AllDelta?
-  expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                    instrumentFormula = ~ Instrument,
-                                    data = SimData)), 
-               expected = 2)
-  # does the function produce right number of elements: coefficients?  
-  expect_equal(length(coef(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                         instrumentFormula = ~ Instrument,
-                                         data = SimData)$RespondentDelta)), 
-               expected = ncol(SimData[,-ncol(SimData)]))
-  expect_equal(length(coef(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                         instrumentFormula = ~ Instrument,
-                                         data = SimData)$AllDelta)), 
-               expected = ncol(SimData[,-ncol(SimData)]))
-  # number of residuals/fitted values
-  expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                    instrumentFormula = ~ Instrument,
-                                    data = SimData)$RespondentDelta[[2]]),
-               expected = length(na.omit(SimData[,1])))
-  expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                    instrumentFormula = ~ Instrument,
-                                    data = SimData)$AllDelta[[2]]),
-               expected = length(na.omit(SimData[,1])))
-  expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                    instrumentFormula = ~ Instrument,
-                                    data = SimData)$RespondentDelta[[3]]),
-               expected = length(na.omit(SimData[,1])))
-  expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
-                                    instrumentFormula = ~ Instrument,
-                                    data = SimData)$AllDelta[[3]]),
-               expected = length(na.omit(SimData[,1])))
-})
 
-test_that("estimateDelta detect error", {
-  # does the function detect error when it should?
-  expect_error(estimateDelta(regressionFormula = Y ~ Covariate, # Omit Treatment
-                             instrumentFormula = ~ Instrument,
-                             data = SimData))
-})
+# STEPS BELOW SAVED FOT LATER TESTS ------------------------------------------
+# 
+# test_that("estimateDelta produce an output of right class", {
+#   # does the function produce an output of right class?
+#   expect_is(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                           instrumentFormula = ~ Instrument,
+#                           data = SimData), 
+#             class = "list")
+#   expect_is(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                           instrumentFormula = ~ Instrument,
+#                           data = SimData)$RespondentDelta, 
+#             class = "lm")
+#   expect_is(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                           instrumentFormula = ~ Instrument,
+#                           data = SimData)$AllDelta, 
+#             class = "lm")
+# })
+# 
+# test_that("estimateDelta have right number of elements", {
+#   # does the function produce both RespondentDelta and AllDelta?
+#   expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                     instrumentFormula = ~ Instrument,
+#                                     data = SimData)), 
+#                expected = 2)
+#   # does the function produce right number of elements: coefficients?  
+#   expect_equal(length(coef(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                          instrumentFormula = ~ Instrument,
+#                                          data = SimData)$RespondentDelta)), 
+#                expected = ncol(SimData[,-ncol(SimData)]))
+#   expect_equal(length(coef(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                          instrumentFormula = ~ Instrument,
+#                                          data = SimData)$AllDelta)), 
+#                expected = ncol(SimData[,-ncol(SimData)]))
+#   # number of residuals/fitted values
+#   expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                     instrumentFormula = ~ Instrument,
+#                                     data = SimData)$RespondentDelta[[2]]),
+#                expected = length(na.omit(SimData[,1])))
+#   expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                     instrumentFormula = ~ Instrument,
+#                                     data = SimData)$AllDelta[[2]]),
+#                expected = length(na.omit(SimData[,1])))
+#   expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                     instrumentFormula = ~ Instrument,
+#                                     data = SimData)$RespondentDelta[[3]]),
+#                expected = length(na.omit(SimData[,1])))
+#   expect_equal(length(estimateDelta(regressionFormula = Y ~ Treatment + Covariate,
+#                                     instrumentFormula = ~ Instrument,
+#                                     data = SimData)$AllDelta[[3]]),
+#                expected = length(na.omit(SimData[,1])))
+# })
+# 
+# test_that("estimateDelta detect error", {
+#   # does the function detect error when it should?
+#   expect_error(estimateDelta(regressionFormula = Y ~ Covariate, # Omit Treatment
+#                              instrumentFormula = ~ Instrument,
+#                              data = SimData))
+# })
